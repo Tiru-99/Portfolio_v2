5c0ba1d490dfe4575537afcc9a5641fda1dd1f73
Tiru-99 <aayushtirmanwar1234@gmail.com>
2026-02-24 21:19:24 +0530

    add : page metadata fixed :

--- app/blogs/[slug]/layout.tsx
+++ app/blogs/[slug]/layout.tsx
@@ -1,24 +1,52 @@
 import type { Metadata } from "next";
-import { getBlogBySlug } from '@/lib/mdx'
+import { getBlogBySlug } from "@/lib/mdx";
 import { notFound } from "next/navigation";
+
 type Props = {
   children: React.ReactNode;
   params: Promise<{ slug: string }>;
 };
 
+export async function generateMetadata({
+  params,
+}: Props): Promise<Metadata> {
+  const { slug } = await params;
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = await params ; 
-  console.log("The slug is " , slug); 
   const post = getBlogBySlug(slug);
-  
-  if(!post){
-    notFound(); 
+
+  if (!post) {
+    notFound();
   }
+
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL; 
+  const url = `${baseUrl}/blog/${slug}`;
+  const imageUrl = `${baseUrl}${post.coverImage}`; 
 
   return {
     title: `${post.title} | Aayush Tirmanwar`,
     description: post.description,
+
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      url: url,
+      siteName: "Aayush Tirmanwar",
+      images: [
+        {
+          url: imageUrl,
+          width: 1200,
+          height: 630,
+        },
+      ],
+      type: "article",
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: post.title,
+      description: post.description,
+      images: [imageUrl],
+    },
   };
 }
 
